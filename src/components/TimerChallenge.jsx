1ade500c7338f4a833ddd66d1be92d516e4ff699
mohamuhsin <muhsinohoho@outlook.com>
2024-06-08 22:27:39 +0300

    we have created timerStarted state and used it condtionally and also used refs to store timer for each component instance

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
+
+    const timer = useRef()
+
+    const [timerStarted, setTimerStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
 
     function handleStart() {
-        setTimeout(() => {
+        timer.current = setTimeout(() => {
             setTimerExpired(true);
         }, targetTime * 1000);
+
+        setTimerStarted(true);
+    }
+
+    function handleStop() {
+        clearTimeout(timer.current);
     }
     return (
         <section className="challenge">
@@ -16,9 +26,11 @@ export default function TimerChallenge({ title, targetTime }) {
                 {targetTime} second{targetTime > 1 ? "s" : null}
             </p>
             <p>
-                <button onClick={handleStart}>Start Challenge</button>
+                <button onClick={timerStarted ? handleStop : handleStart}>
+                    {timerStarted ? "Stop" : "Start"} Challenge
+                </button>
             </p>
-            <p>Time is running / Time Inactive</p>
+            <p className={timerStarted ? 'active' : undefined}>{timerStarted ? "Time is running..." : "Time Inactive"}</p>
         </section>
     );
 }
