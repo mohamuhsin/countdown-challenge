e8518f2e3b16086addb1c6764fb0c80f35e7ca54
mohamuhsin <muhsinohoho@outlook.com>
2024-06-11 00:37:26 +0300

    we shared state across components

--- src/components/ResultsModal.jsx
+++ src/components/ResultsModal.jsx
@@ -1,10 +1,12 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-    { result, targetTime },
+    { targetTime, remainingTime, onReset },
     ref
 ) {
     const dialog = useRef();
+
+    const userLost = remainingTime <= 0;
 
     useImperativeHandle(ref, () => {
         return {
@@ -15,14 +17,14 @@ const ResultModal = forwardRef(function ResultModal(
     });
     return (
         <dialog className="result-modal" ref={dialog}>
-            <h2>You {result}</h2>
+            {userLost && <h2>You Lost</h2>}
             <p>
                 The Target time was <strong> {targetTime} seconds</strong>
             </p>
             <p>
-                You stopped the timer with <strong>X seconds left</strong>
+                You stopped the timer with <strong>{remainingTime}</strong>
             </p>
-            <form method="dialog">
+            <form method="dialog" onClose={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -11,8 +11,11 @@ export default function TimerChallenge({ title, targetTime }) {
 
     if (timeRemaining <= 0) {
         clearInterval(timer.current);
-        setTimeRemaining(targetTime * 1000);
         dialog.current.open();
+    }
+
+    function handleReset() {
+        setTimeRemaining(targetTime * 1000);
     }
 
     function handleStart() {
@@ -28,7 +31,12 @@ export default function TimerChallenge({ title, targetTime }) {
 
     return (
         <>
-            <ResultModal ref={dialog} result="lost" targetTime={targetTime} />
+            <ResultModal
+                ref={dialog}
+                targetTime={targetTime}
+                remainingTime={timeRemaining}
+                onReset={handleReset}
+            />
             <section className="challenge">
                 <h2>{title}</h2>
                 <p className="challenge-time">
